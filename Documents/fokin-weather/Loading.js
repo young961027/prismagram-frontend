3fe04f440b83694e5d3ae2a023b2fdccc44b91ce
loopbackseal <young961027@gmail.com>
2020-01-27 16:42:29 +0900

    test

--- Documents/fokin-weather/Loading.js
+++ Documents/fokin-weather/Loading.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import App from "./App";
 
 
-export default function Loading() {
+function Loading() {
     return <View style={styles.container}>
         <Text style={styles.text}>Getting the Fucking Weather</Text>
     </View>
