6833932512412f978faf774cafc95979ccec48b9
loopbackseal <young961027@gmail.com>
2020-02-07 20:56:39 +0900

    #4.getting the weather

--- Documents/fokin-weather/App.js
+++ Documents/fokin-weather/App.js
@@ -2,17 +2,27 @@ import React from 'react';
 import Loading from './Loading';
 import {Alert} from 'react-native';
 import * as Location from 'expo-location';
+import axios from "axios";
+
+const API_KEY = "39543f76e029ae39ad2b20f072bbe7a5"
 
 
 export default class extends React.Component {
   state = {
     isLoading: true
   };
+  getWeather = async (latitude,longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+  };
   getLocation = async () => {
     try {
       const response = await Location.requestPermissionsAsync();
       const { coords: { latitude, longitude} 
       } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude)
       //Send to API and get weather
     } catch(error) {
       Alert.alert("Can't find you", "So sad");
