2cda12c43af02766091ef870f59eb770420d7301
loopbackseal <young961027@gmail.com>
2020-07-31 14:22:52 +0900

    Footer and Auth UI part 2

--- Documents/prismagram-frontend/src/Components/Footer.js
+++ Documents/prismagram-frontend/src/Components/Footer.js
@@ -1,13 +1,32 @@
 import React from "react";
 import styled from "styled-components";
 
-const Footer = styled.footer``;
+const Footer = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-transform: uppercase;
+  font-weight: 600;
+  font-size: 12px;
+`;
 
-const List = styled.ul``;
+const List = styled.ul`
+  display: flex;
+`;
 
-const ListItem = styled.li``;
+const ListItem = styled.li`
+  &:not(:last-child) {
+    margin-right: 16px;
+  }
+`;
 
-const Link = styled.a``;
+const Link = styled.a`
+  color: ${(props) => props.theme.darkBlueColor};
+`;
+
+const CopyRight = styled.span`
+  color: ${(props) => props.theme.darkGreyColor};
+`;
 
 export default () => (
   <Footer>
@@ -46,5 +65,6 @@ export default () => (
         <Link href="#">language</Link>
       </ListItem>
     </List>
+    <CopyRight>LoopBackStudio {new Date().getFullYear()} &copy;</CopyRight>
   </Footer>
 );
--- Documents/prismagram-frontend/src/Styles/GlobalStyles.js
+++ Documents/prismagram-frontend/src/Styles/GlobalStyles.js
@@ -3,15 +3,19 @@ import reset from "styled-reset";
 
 export default createGlobalStyle`
     ${reset};
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap');
     * {
         box-sizing: border-box;
     }
     body {
         background-color:${(props) => props.theme.bgColor};
         color:${(props) => props.theme.blackColor};
+        font-size: 14px;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";   
+        // 각 운영체제 별로 기본 시스템 폰트를 로드하는 CSS 코드
     }
     a {
         color:${(props) => props.theme.blueColor};
-        text=decoration:none;
+        text-decoration:none;
     }
 `;
