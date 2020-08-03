fb4a3daf045cd3ea32f1007e96b9db15ae6671ca
loopbackseal <young961027@gmail.com>
2020-08-03 17:30:42 +0900

    Import toast and createAccount Mutation

--- Documents/prismagram-frontend/src/Routes/Auth/AuthContainer.js
+++ Documents/prismagram-frontend/src/Routes/Auth/AuthContainer.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import AuthPresenter from "./AuthPresenter";
 import useInput from "../../Hooks/useInput";
 import { useMutation } from "react-apollo-hooks";
-import { LOG_IN } from "./AuthQueries";
+import { LOG_IN, CREATE_ACCOUNT } from "./AuthQueries";
 
 export default () => {
   const [action, setAction] = useState("logIn");
@@ -12,14 +14,45 @@ export default () => {
   const email = useInput("");
 
   const [requestSecret] = useMutation(LOG_IN, {
+    update: (_, { data }) => {
+      const { requestSecret } = data;
+      if (!requestSecret) {
+        toast.error("You don't have an account yet. Create one");
+        setTimeout(() => setAction("sign up"), 2000);
+      }
+    },
     variables: { email: email.value },
   });
 
-  const onLogin = (e) => {
+  const createAccount = useMutation(CREATE_ACCOUNT, {
+    variables: {
+      email: email.value,
+      username: username.value,
+      firstName: firstName.value,
+      lastName: lastName.value,
+    },
+  });
+
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (email !== "") {
-      requestSecret();
-      email.value = "";
+    if (action === "logIn") {
+      if (email !== "") {
+        requestSecret();
+        email.value = "";
+      } else {
+        toast.error("Email is required");
+      }
+    } else if (action === "signUp") {
+      if (
+        email.value !== "" &&
+        username.value !== "" &&
+        firstName.value !== "" &&
+        lastName.value !== ""
+      ) {
+        createAccount();
+      } else {
+        toast.error("All fields are required");
+      }
     }
   };
 
@@ -31,7 +64,7 @@ export default () => {
       firstName={firstName}
       lastName={lastName}
       email={email}
-      onLogin={onLogin}
+      onSubmit={onSubmit}
     />
   );
 };
