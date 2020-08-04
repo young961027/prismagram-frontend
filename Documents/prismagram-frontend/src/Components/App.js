6266484a6ca441f1f35d36bfadc62e710e1653eb
loopbackseal <young961027@gmail.com>
2020-08-04 22:53:18 +0900

    Small fix on frontend

--- Documents/prismagram-frontend/src/Components/App.js
+++ Documents/prismagram-frontend/src/Components/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import styled, { ThemeProvider } from "styled-components";
+import { useQuery } from "react-apollo-hooks";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { useQuery } from "react-apollo-hooks";
 import GlobalStyles from "../Styles/GlobalStyles";
 import Theme from "../Styles/Theme";
 import AppRouter from "./Router";
--- Documents/prismagram-frontend/src/Routes/Auth/AuthContainer.js
+++ Documents/prismagram-frontend/src/Routes/Auth/AuthContainer.js
@@ -1,30 +1,30 @@
 import React, { useState } from "react";
-import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AuthPresenter from "./AuthPresenter";
 import useInput from "../../Hooks/useInput";
 import { useMutation } from "react-apollo-hooks";
 import { LOG_IN, CREATE_ACCOUNT } from "./AuthQueries";
+import { toast } from "react-toastify";
 
 export default () => {
   const [action, setAction] = useState("logIn");
   const username = useInput("");
   const firstName = useInput("");
   const lastName = useInput("");
-  const email = useInput("");
+  const email = useInput("young961027@gmail.com");
 
   const [requestSecret] = useMutation(LOG_IN, {
     update: (_, { data }) => {
       const { requestSecret } = data;
       if (!requestSecret) {
         toast.error("You don't have an account yet. Create one");
-        setTimeout(() => setAction("sign up"), 2000);
+        setTimeout(() => setAction("sign up"), 3000);
       }
     },
     variables: { email: email.value },
   });
 
-  const createAccount = useMutation(CREATE_ACCOUNT, {
+  const [createAccount] = useMutation(CREATE_ACCOUNT, {
     variables: {
       email: email.value,
       username: username.value,
@@ -36,7 +36,7 @@ export default () => {
   const onSubmit = (e) => {
     e.preventDefault();
     if (action === "logIn") {
-      if (email !== "") {
+      if (email.value !== "") {
         requestSecret();
         email.value = "";
       } else {
